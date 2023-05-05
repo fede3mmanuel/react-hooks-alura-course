40fa8015a112a92e77360ebfb49ffa778d6ee19e
Emmanuel Sanchez <fede@fede.website>
2023-05-05 14:29:55 -0600

    Datos usuario #1

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./validaciones";
 
 const  DatosUsuario = () => {
 
@@ -36,7 +37,10 @@ const  DatosUsuario = () => {
           error={false}
           helperText={false && "Ingresa un correo electrónico válido"}
           value={email.value}
-          onChange={ (input) => {setEmail({ value: input.target.value, valid: true}) }}
+          onChange={ (input) => {
+            const email = input.target.value;
+            const valido = validarEmail(email);
+            setEmail({ value: email, valid: valido}) }}
         />
         <TextField
           label="Contraseña"
@@ -45,7 +49,9 @@ const  DatosUsuario = () => {
           margin="dense"
           type="password"
           value={ password.value }
-          onChange={ (input) => setPassword({ value: input.target.value, valid: true }) }
+          onChange={ (input) => {
+            const password = input.target.value;
+            setPassword({ value: password, valid: validarPassword(password) })} }
         />
         <Button variant="contained" type="submit">
           Siguiente
